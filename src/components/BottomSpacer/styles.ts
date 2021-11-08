927efdc68ede4478ccb32129f636e38f65896335
Matheus Boldrini <matheus.souza.boldrini@hotmail.com>
2021-11-08 18:39:24 -0300

    criação do spacer de fundo

---
+++ src/components/BottomSpacer/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+export const Container = styled.TouchableOpacity`
+    width: 100%;
+    height: ${RFValue(100)}px;
+`;
+
