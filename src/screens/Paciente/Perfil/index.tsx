9d1b530d9885cbc033b62d0490f41ea8d93952a4
Matheus Boldrini <matheus.souza.boldrini@hotmail.com>
2021-11-08 18:47:34 -0300

    criação da estética de perfil

--- src/screens/Paciente/Perfil/index.tsx
+++ src/screens/Paciente/Perfil/index.tsx
@@ -1,18 +1,33 @@
 import React, {useEffect, useState}from 'react';
 import {RefreshControl} from 'react-native';
-import {useNavigation } from '@react-navigation/native';
+import {useNavigation, useRoute } from '@react-navigation/native';
 import { 
     Container,
+    WrapGroup,
+    Title,
+    WrapInfo,
+    Spacer,
+    Icone,
+    InfoArea,
+    Description,
+    Info,
 } from './styles';
 
 import { Cabecalho } from '../../../components/Cabecalho';
 import { PacienteHeader } from '../../../components/PacienteHeader';
+import { BottomSpacer } from '../../../components/BottomSpacer';
 
 export function PacientePerfil(){
 
     const navigation = useNavigation();
-
+    const route = useRoute();
     const [refreshing, setRefresh] = useState(false);
+
+    const [data, setData] = useState(route.params);
+
+    useEffect(()=>{
+        console.log(`Recebeu via URL: ${route.params}`);
+    }, []);
 
 
     return(
@@ -20,7 +35,7 @@ export function PacientePerfil(){
             
             <Cabecalho 
                 titulo="Perfil do Paciente"
-                onPress={()=>{}}
+                onPress={()=>{  navigation.navigate("Home" as never)}}
             />
 
             <PacienteHeader 
@@ -28,6 +43,98 @@ export function PacientePerfil(){
                 tipo="Plano Unimed SP"
                 nome="Paulo Muzzy"
             />
+
+            <WrapGroup>
+                <Title>Informações Pessoais</Title>
+
+                <WrapInfo>
+                    <Icone name="user"/>
+                    <InfoArea>
+                        <Description>Nome</Description>
+                        <Info>Descricao</Info>
+                    </InfoArea>
+                </WrapInfo>
+
+                <Spacer/>
+
+                <WrapInfo>
+                    <Icone name="calendar-day"/>
+                    <InfoArea>
+                        <Description>Data de Nascimento</Description>
+                        <Info>01/01/2001</Info>
+                    </InfoArea>
+                </WrapInfo>
+
+                <Spacer/>
+
+                <WrapInfo>
+                    <Icone name="id-card"/>
+                    <InfoArea>
+                        <Description>CPF</Description>
+                        <Info>777.888.777-99</Info>
+                    </InfoArea>
+                </WrapInfo>
+
+            </WrapGroup>
+
+            <WrapGroup>
+                <Title>Informações de Contato</Title>
+                
+                <WrapInfo>
+                    <Icone name="whatsapp"/>
+                    <InfoArea>
+                        <Description>Celular</Description>
+                        <Info>(27) 98888-5555</Info>
+                    </InfoArea>
+                </WrapInfo>
+
+                <Spacer/>
+
+                <WrapInfo>
+                    <Icone name="phone"/>
+                    <InfoArea>
+                        <Description>Tel. Recado</Description>
+                        <Info>(27) 98888-5555</Info>
+                    </InfoArea>
+                </WrapInfo>
+
+                <Spacer/>
+
+                <WrapInfo>
+                    <Icone name="envelope"/>
+                    <InfoArea>
+                        <Description>E-mail</Description>
+                        <Info>contato@birinelson.com</Info>
+                    </InfoArea>
+                </WrapInfo>
+
+                <Spacer/>
+
+                <WrapInfo>
+                    <Icone name="map-pin"/>
+                    <InfoArea>
+                        <Description>Endereço</Description>
+                        <Info>Rua dos Alfeneiros Nº 156</Info>
+                    </InfoArea>
+                </WrapInfo>
+
+            </WrapGroup>
+
+            <WrapGroup>
+                <Title>Informações de Médicas</Title>
+                
+                <WrapInfo>
+                    <Icone name="star-of-life"/>
+                    <InfoArea>
+                        <Description>Comorbidade</Description>
+                        <Info>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum libero sit amet orci pretium ultrices. Maecenas vitae nunc eget est pretium ullamcorper. Vestibulum eu metus in velit bibendum venenatis sit amet quis est. In molestie varius aliquam. Nam tempor pulvinar lorem. Suspendisse venenatis fringilla tortor vel varius.</Info>
+                    </InfoArea>
+                </WrapInfo>
+
+            </WrapGroup>
+
+
+            <BottomSpacer/>
               
 
         </Container>
--- src/screens/Paciente/Perfil/styles.ts
+++ src/screens/Paciente/Perfil/styles.ts
@@ -6,3 +6,58 @@ export const Container = styled.ScrollView`
     flex: 1;
     background-color: ${({theme}) => theme.colors.primary};
 `;
+
+export const WrapGroup = styled.View`
+    background-color: ${({theme}) => theme.colors.secondary};
+    margin: 0 ${({theme}) => theme.margin.lateral}px;
+    border-radius: ${({theme}) => theme.bordas.padrao}px;
+    margin-bottom: ${({theme}) => theme.margin.bottom}px;
+    padding: ${RFValue(10)}px ${({theme}) => theme.padding.lateral_half}px;
+`;
+
+export const Title = styled.Text`
+    font-size: ${RFValue(16)}px;
+    font-family: ${({theme}) => theme.fonts.bold};
+    color: ${({theme}) => theme.colors.shape};
+`;
+
+export const WrapInfo = styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+    margin: 5px 0;
+    padding-bottom: ${RFValue(3)}px;
+`;
+
+export const Spacer = styled.View`
+    width: 100%;
+    border-bottom-color: ${({theme}) => theme.colors.primary};
+    border-bottom-width: 1px;
+`;
+
+export const Icone = styled(FontAwesome5)`
+    font-size: ${RFValue(28)}px;
+    color: ${({theme}) => theme.colors.shape};
+    min-width: ${RFValue(37)}px;
+`;
+
+export const InfoArea = styled.View`
+    margin-left: ${RFValue(10)}px;
+`;
+
+export const Description = styled.Text`
+    color: ${({theme}) => theme.colors.shape};
+    font-family: ${({theme}) => theme.fonts.thin};
+    font-size: ${RFValue(13)}px;
+`;
+
+export const Info = styled.Text`
+    color: ${({theme}) => theme.colors.shape};
+    font-family: ${({theme}) => theme.fonts.bold};
+    font-size: ${RFValue(13)}px;
+    padding-right: ${RFValue(15)}px;
+`;
+
+
+
+
