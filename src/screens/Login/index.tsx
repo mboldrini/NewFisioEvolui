42ddda7047fd4f719b0913c7bda908300296ba4a
Matheus Boldrini <matheus.souza.boldrini@hotmail.com>
2021-12-25 16:40:45 -0300

    inicio do login..

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ButtonGoogle } from '../../components/Forms/ButtonGoogle/Index';
 import {useNavigation} from '@react-navigation/native';
 import { 
@@ -9,16 +9,52 @@ import {
     LoadingIcon
 } from './styles';
 
+import * as Google from 'expo-google-app-auth';
 
 export function Login(){
 
+    const [loading, setLoading] = useState(false);
+
     const navigation = useNavigation();
 
-    function handleLogin(){
-        // navigation.reset({
-        //     routes:[{name:'MainTab'}]
-        // });
-        alert("apertotu");
+    async function signInWithGoogle(){
+        try{
+            setLoading(true);
+
+            const result = await Google.logInAsync({
+                androidClientId: '19918590573-iegpvh191l2qeoc24i9nvf0jbii6dpk6.apps.googleusercontent.com',
+                scopes: ['profile', 'email']
+            });
+
+            if(result.type === 'success'){
+                const userLogged = {
+                    id: String(result.user.id),
+                    email: result.user.email,
+                    name: result.user.name!,
+                    photo: result.user.photoUrl!,
+                }
+
+                alert(JSON.stringify(userLogged));
+
+            }
+
+            setLoading(false);
+
+        }catch(error){
+            throw(`Erro: ${error}`);
+        }finally{
+            setLoading(false);
+        }
+    }
+
+    async function handleLogin(){
+        try{
+            await signInWithGoogle();
+        }catch(e){
+            alert(e);
+        }finally{
+            setLoading(false);
+        }
     }
 
     return(
@@ -27,13 +63,18 @@ export function Login(){
                 <Photo source={require('../../assets/logo.png')}/>
             </WrapLogo>
 
-            <WrapInput>
-                <ButtonGoogle
-                    onPress={()=>{handleLogin()}}
-                />
-            </WrapInput>
+            { !loading &&
+                <WrapInput>
+                    <ButtonGoogle
+                        onPress={()=>{signInWithGoogle()}}
+                    />
+                </WrapInput>
+            }
+          
             
-            {/* <LoadingIcon size="large" color="#FFFFFF"/>  */}
+            { loading &&
+                <LoadingIcon size="large" color="#FFFFFF"/>            
+            }
         
         </Container>
     );
