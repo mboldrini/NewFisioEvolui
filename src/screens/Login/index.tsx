968a247d2d761e3de7b686e52da00d7917430367
Matheus Boldrini <matheus.souza.boldrini@hotmail.com>
2021-12-27 21:32:53 -0300

    login google finalmente funcionando

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Alert } from 'react-native';
+//import { Alert } from 'react-native';
 import { ButtonGoogle } from '../../components/Forms/ButtonGoogle/Index';
 import { useNavigation } from '@react-navigation/native';
+import * as AuthSession from 'expo-auth-session';
 import { 
     Container,
     WrapLogo,
@@ -9,31 +10,60 @@ import {
     WrapInput,
     LoadingIcon
 } from './styles';
+import { Text } from 'react-native';
 
-import { useAuth } from '../../hooks/auth';
+type AuthResponse = {
+    type: string;
+    params: {
+        access_token: string;
+    }
+}
 
 export function Login(){
 
-
     const [loading, setLoading] = useState(false);
+    const [token, setToken] = useState(null);
+    const [userInfos, setUserInfos] = useState(null);
 
     const navigation = useNavigation();
 
-    const { user, signInWithGoogle } = useAuth();
-    
-    async function handleSignInWithGoogle(){
-        setLoading(true);
-        try{
-            await signInWithGoogle();
+    async function newhandleSignInWithGoogle(){
 
-        }catch(error){
-            console.log("ERRO");
-            console.log(error);
-            Alert.alert('Não foi possível conectar a conta Google');
-        }finally{
-            setLoading(false);
+        setLoading(true);
+
+        const CLIENT_ID = '19918590573-m2k3b72f7jq816hvu3rcucov4itvjvji.apps.googleusercontent.com';
+        const REDIRECT_URL = 'https://auth.expo.io/@mboldrini/fisioevolui';
+        const RESPONSE_TYPE = 'token';
+        const SCOPE = encodeURI('profile email');
+
+        const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+        const { type, params } = await AuthSession.startAsync({ authUrl }) as AuthResponse;
+
+        if(type === 'success'){
+            console.log("SUCESSO!");
+            //loadProfile(params.access_token);
+            setToken(params.access_token);
+        }else{
+            alert("erro");
         }
     }
+
+    async function loadProfile(tkn: string){
+        console.log("vai pegar os parametrros via token");
+        const response = await fetch(`https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=${token}`);
+        const userInfo = await response.json();
+        console.log(userInfo);
+
+        setUserInfos(userInfo);
+
+        setLoading(false);
+    }
+
+    useEffect(()=>{
+        if(token != null){
+            loadProfile(token);
+        }
+    }, [token]);
 
     return(
         <Container>
@@ -44,11 +74,15 @@ export function Login(){
             { !loading &&
                 <WrapInput>
                     <ButtonGoogle
-                        onPress={handleSignInWithGoogle}
+                        onPress={newhandleSignInWithGoogle}
                     />
                 </WrapInput>
             }
           
+            { userInfos &&
+                <Text>Olá {userInfos.name}</Text>
+            }
+
             
             { loading &&
                 <LoadingIcon size="large" color="#FFFFFF"/>            
