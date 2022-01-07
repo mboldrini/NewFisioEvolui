2f15c464cfdc5430cf4de71ab0c85ed26ce27e76
Matheus Boldrini <matheus.souza.boldrini@hotmail.com>
2022-01-07 20:56:07 -0300

    novas opções de cores e tamanho do botão comum

--- src/components/Forms/Button/styles.ts
+++ src/components/Forms/Button/styles.ts
@@ -1,14 +1,29 @@
 import { RFValue } from 'react-native-responsive-fontsize';
-import styled from "styled-components/native";
+import styled, {css} from 'styled-components/native';
 import { TouchableOpacity } from 'react-native';
 import { RectButton } from "react-native-gesture-handler";
 
-export const Container = styled(TouchableOpacity)`
+interface Props{
+    type?: string;
+}
+
+export const Container = styled(TouchableOpacity)<Props>`
     background-color: ${({theme}) => theme.colors.button_ok};
     border-radius: ${({theme}) => theme.bordas.padrao}px;
     align-items: center;
     justify-content: center;
     height: ${RFValue(45)}px;
+
+    ${({ type }) => type === "ok" && css `
+        background-color: ${({theme}) => theme.colors.success};
+        height: ${RFValue(55)}px;
+    `};
+
+    ${({ type }) => type === "cancel" && css `
+        background-color: ${({theme}) => theme.colors.status_cancelado};
+        height: ${RFValue(55)}px;
+    `};
+
 `;
 
 export const Title = styled.Text`
