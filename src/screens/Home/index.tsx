6eae204532d70a76077fc41d94869c7cc4bb5292
Matheus Boldrini <matheus.souza.boldrini@hotmail.com>
2022-04-20 11:43:00 -0300

    carregando lista de pacientes

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,42 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { PacienteList } from '../../components/PacienteList';
 import {useNavigation} from '@react-navigation/native';
+import { FlatList } from 'react-native';
+// API
+import { api } from '../../global/api';
+// REDUX
+import { useSelector } from 'react-redux';
+import { State } from '../../state';
+import LottieView from 'lottie-react-native';
+
 import { 
     Container,
     Header,
     Titulo,
-    Icon
+    Icon,
+    Wrap,
+    WrapLoading,
+    TextoLoading,
+
+    WrapScroll
+
 } from './styles';
+
+interface IPatient{
+    id: number
+    nome: string,
+    tipoAtendimento: string,
+    logradouro: string
+}
 
 
 export function Home(){
 
     const navigation = useNavigation();
+
+    const apiState = useSelector((state: State) => state.apiReducer);
+
+    const [patientList, setPatientList] = useState<IPatient[]>([]);
 
     function handleNavigate(numero: number){
         navigation.navigate('PacientePerfil' as number,{
@@ -19,42 +44,25 @@ export function Home(){
         });
     }
 
-    let pacienteList = [
-        {
-            companyIcon:"hospital",
-            companyName:"Plano Amil 500FG",
-            lastConsult:"01/11/2021",
-            personName:"Armelio Silva Boetchi",
-            address:"Rua Jataí Nº 358"
-        },{
-            companyIcon:"hospital",
-            companyName:"Plano Amil 500FG",
-            lastConsult:"01/11/2021",
-            personName:"Armelio Silva Boetchi",
-            address:"Rua Jataí Nº 358",
-        },
-        {        
-            companyIcon:"money-bill-wave",
-            companyName:"Particular",
-            lastConsult:"08/09/2021",
-            personName:"John Cena",
-            address:"Avenida Carlos Lindemberg",
-        },
-        {
-            companyIcon:"hospital",
-            companyName:"Plano Unimed",
-            lastConsult:"28/07/2021",
-            personName:"Senor Abravanel",
-            address:"Rua Roberto Schwartzmann",
-        },
-        {
-            companyIcon:"hospital",
-            companyName:"Plano Unimed",
-            lastConsult:"28/07/2021",
-            personName:"Senor Abravanel",
-            address:"Rua Roberto Schwartzmann",
-        }
-    ];
+    async function GetPatientList(){
+        console.group("GetPatientList()");
+
+        await api(apiState.token).post('/paciente/all', '').then(res=>{
+
+            setPatientList(res.data);
+            console.log("ok?");
+            console.log(res.data);
+
+        }).catch(err=>{
+            console.error(err);
+        })
+
+        console.groupEnd();
+    }
+
+    useEffect(()=>{
+       GetPatientList();
+    },[]);
 
     return(
         <Container>
@@ -63,21 +71,42 @@ export function Home(){
                 <Titulo>Pesquisar nome do paciente</Titulo>
                 <Icon name="search"/>
             </Header>
+ 
+            { patientList.length > 0 &&
+              <FlatList 
+                    data={patientList}
+                    keyExtractor={pct => pct.id+""}
+                    renderItem={({item}) =>(
+                        <PacienteList
+                            key={item.id}
+                            companyIcon={"hospital"}/*{item.companyIcon}*/
+                            companyName={ item.tipoAtendimento }
+                            // lastConsult={"01/01/2001"}
+                            personName={ item.nome }
+                            address={ item.logradouro }
+                            onPress={()=>{console.log("aa")}}
+                        />
+                    )}
+                    
+                />
+            }
 
-            
-            { pacienteList.length > 0 && pacienteList.map((item, key) =>{
-                return(
-                    <PacienteList
-                        key={key}
-                        companyIcon={item.companyIcon}
-                        companyName={item.companyName}
-                        lastConsult={item.lastConsult}
-                        personName={item.personName}
-                        address={item.address}
-                        onPress={()=>{handleNavigate(key)}}
-                    />
-                )
-            }) }
+            { patientList.length < 1 &&
+                <Wrap>
+                    <WrapLoading>
+                        <LottieView
+                            source={require('../../assets/loadingAnimado250.json')}
+                            autoSize={false}
+                            autoPlay
+                            loop
+                        />
+                    </WrapLoading>
+                    <TextoLoading>Carregando lista de pacientes...</TextoLoading>
+                </Wrap>
+            }
+
+
+
 
         </Container>
     )
