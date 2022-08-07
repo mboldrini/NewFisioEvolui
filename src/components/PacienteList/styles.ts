4b6deb04951b54aeb38d23556c6e4b98aa841255
Matheus Boldrini <matheus.souza.boldrini@hotmail.com>
2022-08-07 10:30:21 -0300

    listando pacientes + updates

--- src/components/PacienteList/styles.ts
+++ src/components/PacienteList/styles.ts
@@ -5,25 +5,23 @@ import { RectButton } from "react-native-gesture-handler";
 
 export const Container = styled(RectButton)`
     background-color: ${({theme}) => theme.colors.shape};
-    margin-left: ${({theme}) => theme.margin.lateral}px;
-    margin-right: ${({theme}) => theme.margin.lateral}px;
+    margin: 0 ${({theme}) => theme.margin.lateral_half}px;
     border-radius: ${({theme}) => theme.bordas.padrao}px;
     flex-direction:column;
-    padding: 10px 15px;
+    padding: ${RFValue(5)}px ${RFValue(15)}px;
     margin-bottom: ${({theme}) => theme.margin.bottom}px;
-    border-width: 1px;
-    border-color: #f5f5f5;
-    border-bottom-width: 1px;
+
     shadow-color: #000;
     shadow-offset: {width: 0px};
     shadow-offset: {height: 2px};
     shadow-opacity: 0.8;
     shadow-radius: 1px;
-    elevation: 1;
+    elevation: 5;
+
 `;
 
 export const Icone = styled(FontAwesome5)`
-    font-size: ${RFValue(12)}px;
+    font-size: ${RFValue(10)}px;
     color: ${({theme}) => theme.colors.secondary};
     padding: 0 5px 0 0;
 `;
@@ -31,18 +29,18 @@ export const Icone = styled(FontAwesome5)`
 export const Header = styled.View`
     flex-direction: row;
     justify-content: space-between;
-    padding: 5px 0 0 0;
+    padding: 2px 0 0 0;
 `;
 
 export const Company = styled.Text`
-    font-size: ${RFValue(12)}px;
+    font-size: ${RFValue(10)}px;
     color: ${({theme}) => theme.colors.text};
     font-family: ${({theme}) => theme.fonts.thin};
 
 `;
 
 export const LastDate = styled.Text`
-    font-size: ${RFValue(12)}px;
+    font-size: ${RFValue(10)}px;
     color: ${({theme}) => theme.colors.text};
     font-family: ${({theme}) => theme.fonts.thin};
 
@@ -52,14 +50,14 @@ export const PersonWrap = styled.View``;
 
 export const PersonName = styled.Text`
     font-family: ${({theme}) => theme.fonts.bold};
-    font-size: ${RFValue(18)}px;
+    font-size: ${RFValue(14)}px;
     color: #000000;
 `;
 
 export const AddressWrap = styled.View``;
 
 export const Address = styled.Text`
-    font-size: ${RFValue(14)}px;
+    font-size: ${RFValue(10)}px;
     color: ${({theme}) => theme.colors.text};
     font-family: ${({theme}) => theme.fonts.regular};
     
