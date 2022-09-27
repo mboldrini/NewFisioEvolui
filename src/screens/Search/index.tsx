cc034fc66659e9f575affeb80a2efbf04f051712
Matheus Boldrini <matheus.souza.boldrini@hotmail.com>
2022-09-27 18:58:11 -0300

    search

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -1,13 +1,89 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { Cabecalho } from '../../components/Cabecalho';
+import React, { useEffect, useState } from 'react';
+import { FlatList, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { 
-    Container
+    Container,
+    SearchInput,
+    WrapCabecalho,
+    Header,
+    WrapBtn,
+    Icone,
+
+    WrapOpcoesList,
+    BtnArea,
+    BtnText,
+
+    Wrap,
+    LoadingIcon
 } from './styles';
 
 export function Search(){
+
+    const opcoes = [
+        {nome: "Nome", slug:"nome" },
+        {nome: "Email", slug:"email" },
+        {nome: "CPF", slug:"cpf" },
+        {nome: "Endereço", slug:"endereco" },
+        {nome: "Telefone", slug:"telefone" },
+    ];
+
+    const [opcao, setOpcao] = useState('nome');
+    const [text, onChangeText] = useState("");
+
+    const [loading, setLoading] = useState(false);
+
+    async function HandlePesquisa(){
+        console.group("HandlePesquisa");
+
+
+        console.log("vai pesquisar por: "+ text  +" - "+ opcao);
+
+
+        console.groupEnd();
+    }
+
+
+    useEffect(()=>{
+        onChangeText("");
+    }, [opcao]);
+
     return(
-        <Container>
-        </Container>
+<SafeAreaView style={{flex: 1, backgroundColor: '#63C2D1'}}>
+    <Container >
+
+        <WrapCabecalho>
+            <Header>
+                <SearchInput placeholder="Pesquisar..." autoCapitalize="words" autoCorrect={false} onChangeText={onChangeText} value={text} />
+                <WrapBtn onPress={()=> HandlePesquisa() }>
+                    <Icone name="search" />
+                </WrapBtn>
+            </Header>
+
+            <WrapOpcoesList>
+                <FlatList
+                    data={opcoes}
+                    keyExtractor={(item) => item.nome}
+                    horizontal={true}
+                    renderItem={({item}) =>{
+                        return (
+                            <BtnArea enabled={ item.slug == opcao ? true : false } onPress={()=> setOpcao(item.slug) }>
+                                <BtnText enabled={ item.slug == opcao ? true : false }>{item.nome}</BtnText>
+                            </BtnArea>
+                        )} 
+                    }
+                />
+            </WrapOpcoesList>
+        </WrapCabecalho>
+
+
+        {loading &&
+            <Wrap>
+                <LoadingIcon size="large" color="#FFFFFF"/>   
+            </Wrap>
+        }
+      
+
+    </Container>
+</SafeAreaView>
     )
 }
--- src/screens/Search/styles.ts
+++ src/screens/Search/styles.ts
@@ -1,6 +1,88 @@
-import styled from "styled-components/native";
+import { RFValue } from 'react-native-responsive-fontsize';
+import { FontAwesome5 } from '@expo/vector-icons';
+import styled, {css} from "styled-components/native";
+import { TextInput } from 'react-native';
+import { DefaultAppValues } from '../../global/styles/theme';
+
+interface IProps{
+    enabled: boolean;
+}
 
 export const Container = styled.View`
     flex: 1;
     background-color: ${({theme}) => theme.colors.primary};
 `;
+
+export const WrapCabecalho = styled.View``;
+
+export const Header = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    height: 50px;
+    margin-top: ${RFValue(10)}px;
+`;
+
+export const SearchInput = styled(TextInput)`
+    background-color: ${({theme}) => theme.colors.input_background};
+    border-radius: ${RFValue(25)}px;
+    height: ${RFValue(40)}px;
+    margin: 0 ${({theme}) => theme.padding.lateral_half}px;
+    color: #000000;
+    border-width: 1px;
+    border-color: ${({theme}) => theme.colors.input_background};
+    border-bottom-width: 1px;
+    padding-left: 10px;
+    font-size: ${RFValue(14)}px;
+    flex: 1;
+`;
+
+export const WrapOpcoesList = styled.View`
+    margin: 0 ${RFValue(DefaultAppValues.margin.lateral_half)}px;
+    margin-top: ${RFValue(5)}px;
+`;
+
+
+export const WrapBtn = styled.TouchableOpacity``;
+
+export const Icone = styled(FontAwesome5)`
+    font-size: ${RFValue(25)}px;
+    color: ${({theme}) => theme.colors.shape };
+    padding-right: ${RFValue(5)}px;
+    align-items: center;
+    margin-right: ${RFValue(12)}px;
+`;
+
+export const BtnArea = styled.TouchableOpacity<IProps>`
+    background-color: ${({theme}) => theme.colors.secondary_light};
+    margin: 0 ${RFValue(5)}px;
+    padding: ${RFValue(5)}px ${RFValue(10)}px;
+    border-radius: ${RFValue(DefaultAppValues.bordas.padrao)}px;
+    align-items: center;
+    justify-content: center;
+
+    ${({ enabled }) => enabled == true && css `
+        background-color: ${({theme}) => theme.colors.text_dark}; 
+        border: 1px solid ${({theme}) => theme.colors.text_dark};
+    `};
+`;
+
+export const BtnText = styled.Text<IProps>`
+    font-size: ${RFValue(12)}px;
+    font-family: ${({theme}) => theme.fonts.regular};
+    color: ${({theme}) => theme.colors.title};
+    
+    ${({ enabled }) => enabled == true && css `
+        color: #ffffff;
+    `};
+`;
+
+
+export const Wrap = styled.View`
+    margin-top: ${RFValue(15)}px;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+`;
+
+export const LoadingIcon = styled.ActivityIndicator``;
